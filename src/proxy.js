35dd89aca9151ea983ef150bc4a858da6801b862
Rav3d_ <palominodennys@gmail.com>
2026-06-11 00:49:55 -0500

    refactor: rename deprecated middleware to proxy for Next.js 16 compat

--- src/proxy.js
+++ src/proxy.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 
-export function middleware(request) {
+export function proxy(request) {
   const session = request.cookies.get("admin_session")?.value;
   const { pathname } = request.nextUrl;
 
